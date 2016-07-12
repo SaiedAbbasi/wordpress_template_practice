21436847542bd6f25a5e688bbdbead44922088ba
SaiedAbbasi <saiedabbasijr@gmail.com>
2016-07-12 15:06:04 -0400

    nav menus, page template, styles

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<meta name="viewport" content="width=device-width">
 		<title><?php bloginfo('name'); ?></title>
-		<?php wp_head(); ?> <!--hookable-->
+		<?php wp_head(); ?>
 	</head>
 	
 <body <?php body_class(); ?>>
@@ -13,7 +13,21 @@
 	
 		<!-- site-header -->
 		<header class="site-header">
-			<!-- need a logo, about 40% then float left nav, replace name with the logo, or float desc. an oppt... -->
+		<!-- need a logo, about 40% then float left nav, replace name with the logo, or float desc. an oppt... -->
 			<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
 			<h5><?php bloginfo('description'); ?></h5>
+			
+			<nav class="site-nav">
+				
+				<?php
+				
+				$args = array(
+					'theme_location' => 'primary'
+				);
+				
+				?>
+				
+				<?php wp_nav_menu(  $args ); ?>
+			</nav>
+			
 		</header><!-- /site-header -->
